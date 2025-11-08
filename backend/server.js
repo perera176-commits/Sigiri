effae1ab85b3b5a22cb8a1e7c38c860687351ba1
t <t>
2025-11-08 22:15:05 +0400

    Connect backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/sigiri';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
-  res.send('Sigiri backend is running!');
+  res.send('Sigiri backend is running and connected to MongoDB!');
 });
 
 app.listen(PORT, () => {
